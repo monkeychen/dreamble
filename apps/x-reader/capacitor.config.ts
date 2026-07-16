93012c96fc889096fdba9b7dc658081c2ee47cd5
monkeychen <cza55007@gmail.com>
2026-07-16 18:04:07 +0800

    feat: setup capacitor android, update intent-filter for open-with, bump version to 1.0.1

--- apps/x-reader/capacitor.config.ts
+++ apps/x-reader/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.chenzhian.xreader',
+  appId: 'com.simiam.xreader',
   appName: 'x-reader',
   webDir: 'dist'
 };
